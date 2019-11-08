6f088ac4b35fb0f1673651eb69a29e1c992b3aeb
dima_ksecnhyn <ahafonova.kateryna@gmail.com>
2019-11-08 16:05:35 +0200

    task Everad

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '../sass/header.scss';
+import '../sass/styles.scss';
+import  '../sass/danges_section.scss';
+import '../sass/effectSection2.scss';
+import '../sass/Section3TabletsMikozin.scss';
+import  '../sass/footer.scss';
+import  '../sass/media.scss';
+
+
+
+
